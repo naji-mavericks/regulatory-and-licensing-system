1276e05689a9f285e8d1df184f04a362fc0b0f65
Mohamed Najiullah <najiullah@mavericks-consulting.com>
2026-04-28 22:38:47 +0800

    feat: nest operator routes under OperatorLayout shell (#25)

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
+import OperatorLayout from './components/OperatorLayout'
 import ApplicationListPage from './pages/ApplicationListPage'
 import SubmitApplicationPage from './pages/SubmitApplicationPage'
 import ApplicationDetailPage from './pages/ApplicationDetailPage'
@@ -8,9 +9,15 @@ import ResubmissionPage from './pages/ResubmissionPage'
 export const router = createBrowserRouter([
   { path: '/', element: <LoginPage /> },
   { path: '/login', element: <LoginPage /> },
-  { path: '/operator', element: <ApplicationListPage /> },
-  { path: '/operator/applications', element: <ApplicationListPage /> },
-  { path: '/operator/applications/:id', element: <ApplicationDetailPage /> },
-  { path: '/operator/applications/:id/resubmit', element: <ResubmissionPage /> },
-  { path: '/operator/apply', element: <SubmitApplicationPage /> },
+  {
+    path: '/operator',
+    element: <OperatorLayout />,
+    children: [
+      { index: true, element: <ApplicationListPage /> },
+      { path: 'applications', element: <ApplicationListPage /> },
+      { path: 'applications/:id', element: <ApplicationDetailPage /> },
+      { path: 'applications/:id/resubmit', element: <ResubmissionPage /> },
+      { path: 'apply', element: <SubmitApplicationPage /> },
+    ],
+  },
 ])

8a038d45f1e3d0bb2b427f537cac7e87f0cdfd91
Mohamed Najiullah <najiullah@mavericks-consulting.com>
2026-04-29 12:57:13 +0800

    feat: add officer routes under OfficerLayout (#16)

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import OperatorLayout from './components/OperatorLayout'
+import OfficerLayout from './components/OfficerLayout'
 import ApplicationListPage from './pages/ApplicationListPage'
 import SubmitApplicationPage from './pages/SubmitApplicationPage'
 import ApplicationDetailPage from './pages/ApplicationDetailPage'
 import ResubmissionPage from './pages/ResubmissionPage'
+import OfficerApplicationListPage from './pages/OfficerApplicationListPage'
+import OfficerApplicationDetailPage from './pages/OfficerApplicationDetailPage'
 
 export const router = createBrowserRouter([
   { path: '/', element: <LoginPage /> },
@@ -18,6 +21,15 @@ export const router = createBrowserRouter([
       { path: 'applications/:id', element: <ApplicationDetailPage /> },
       { path: 'applications/:id/resubmit', element: <ResubmissionPage /> },
       { path: 'apply', element: <SubmitApplicationPage /> },
+    ],
+  },
+  {
+    path: '/officer',
+    element: <OfficerLayout />,
+    children: [
+      { index: true, element: <OfficerApplicationListPage /> },
+      { path: 'applications', element: <OfficerApplicationListPage /> },
+      { path: 'applications/:id', element: <OfficerApplicationDetailPage /> },
     ],
   },
 ])
